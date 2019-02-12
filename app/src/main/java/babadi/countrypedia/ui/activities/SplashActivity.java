3770d95c7a2403b42be8c312276c67c2081d212f
Babadi <Babadi@admins-Mac-mini.local>
2019-02-12 23:57:46 +0430

    add project dependencies

---
+++ app/src/main/java/babadi/countrypedia/ui/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package babadi.countrypedia.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import babadi.countrypedia.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
---
+++ app/src/main/java/babadi/countrypedia/util/AppController.java
@@ -0,0 +1,4 @@
+package babadi.countrypedia.util;
+
+public class AppController {
+}
